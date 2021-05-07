3b74372781e5a977d3cc48298700c524e50f467b
fabidmc <fabiola.dlmorac@comunidad.unam.mx>
2021-05-06 23:56:43 -0500

    Front-end

---
+++ src/main/java/com/DelaMora/CarRental/configuration/SecurityConfiguration.java
@@ -0,0 +1,14 @@
+package com.DelaMora.CarRental.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfiguration extends WebMvcConfigurer {
+
+
+
+
+
+}
